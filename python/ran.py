58c2e3bffbebc1d73bc147f3eb90163263859e15
davirandos <lucctyte@gmail.com>
2026-06-19 12:13:36 -0300

    Organiza o repositório e adiciona o quarto período.

--- python/ran.py
+++
@@ -1,9 +0,0 @@
-# importando a biblioteca random
-from random import randrange
-
-nome = input('Digite seu nome: ')
-
-# escolhendo um número aleatório
-numero_aleatorio = randrange(1000, 9998, 2)
-
-print(f'Olá {nome}, seu número aleatório é: {numero_aleatorio}')
---
+++ readme.md
@@ -0,0 +1,5 @@
+Aqui estou armazenando os exercícios/atividades feitas em sala de aula. 
+
+A organização tem sido feita por semestres.
+
+Até o momento, na faculdade, tive contato com tecnologias como C e Java.
